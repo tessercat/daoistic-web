30fb26d1ea8c22be9f02b1570875a7a0ba3f25df
Peter Christensen <tessercat@pakaa.net>
2019-03-21 09:22:36 -0600

    Ignore book data import readme file.

--- daoistic/management/commands/importdaoistic.py
+++ daoistic/management/commands/importdaoistic.py
@@ -164,7 +164,7 @@ class Command(BaseCommand):
 
         # Get and create ctext book/chapter data.
         for data_glob in glob.glob(os.path.join(data_dir, 'ctext', '*.md')):
-            if data_glob == 'README.md':
+            if data_glob.endswith('README.md'):
                 continue
             title_data = _get_title_data(
                 os.path.join(data_dir, 'ctext', data_glob)

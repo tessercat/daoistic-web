301df278cf98b910986290792b924aadd2048850
Peter Christensen <tessercat@pakaa.net>
2019-03-21 09:45:20 -0600

    Add daoistic urls.

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from django.urls import path
 
 
+admin.site.site_header = 'Daoistic administration'
+admin.site.site_title = 'Daoistic'
+
 urlpatterns = [
+    path('', include('daoistic.urls')),
+    path('unihan/', include('unihan.urls')),
     path('admin/', admin.site.urls),
 ]

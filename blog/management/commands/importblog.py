6a8b1663c4068dc42be6b59beac3e7ffd3510080
Peter Christensen <peter@daoistic.ca>
2020-04-03 10:45:40 -0600

    Improve blog update.

--- blog/management/commands/importblog.py
+++ blog/management/commands/importblog.py
@@ -1,7 +1,8 @@
 """ Management utility to import blog from markdown. """
-from datetime import datetime, timedelta, timezone
-from shutil import copyfile
+from datetime import date
+import filecmp
 import os
+from shutil import copyfile
 from django.core.management.base import BaseCommand
 from django.conf import settings
 import sh
@@ -18,38 +19,22 @@ class Command(BaseCommand):
     blog_dir = os.path.join(book_dir, 'blog')
     img_dir = os.path.join(settings.BASE_DIR, 'blog', 'static', 'blog', 'img')
 
-    def handle(self, *args, **options):
-        """ Import blog data. """
-
-        # Get entry objects dict, delete objects with no dir.
-        entry_dirs = os.listdir(self.blog_dir)
-        entries = {}  # A dict of existing objects keyed by slug.
-        for entry in Entry.objects.all():
-            if entry.slug not in entry_dirs:
-                entry.delete()
-                print('Deleted', entry)
-            else:
-                entries[entry.slug] = entry
-
-        # Create missing entries, update existing.
-        for slug in entry_dirs:
-            if slug in entries:
-                self._update_entry(entries[slug])
-            else:
-                self._create_entry(slug)
-
     def _cp_static(self, slug):
         """ Copy card and header images to the blog app's static dir. """
-        copyfile(
-            os.path.join(self.blog_dir, slug, 'card.jpg'),
-            os.path.join(self.img_dir, '%s-128.jpg' % slug)
-        )
-        copyfile(
-            os.path.join(self.blog_dir, slug, 'header.jpg'),
-            os.path.join(self.img_dir, '%s.jpg' % slug)
-        )
+        changed = False
+        src = os.path.join(self.blog_dir, slug, 'card.jpg')
+        dst = os.path.join(self.img_dir, '%s-128.jpg' % slug)
+        if not filecmp.cmp(src, dst):
+            copyfile(src, dst)
+            changed = True
+        src = os.path.join(self.blog_dir, slug, 'header.jpg')
+        dst = os.path.join(self.img_dir, '%s.jpg' % slug)
+        if not filecmp.cmp(src, dst):
+            copyfile(src, dst)
+            changed = True
+        return changed
 
-    def _create_entry(self, slug):
+    def _create(self, slug):
         """ Create a new blog entry. """
         first_published = self._first_published(slug)
         last_update = self._last_update(slug)
@@ -66,41 +51,34 @@ class Command(BaseCommand):
                 title=title,
             )
             self._cp_static(slug)
-            print('Created', entry)
+            print('Created', entry.slug)
 
-    def _git_log_date(self, slug, filename, reverse=False):
-        """ Return a datetime for the file from git log. """
+    def _date(self, slug, filename, reverse=False):
+        """ Return a date for the file from git log. """
         cmd = [
             '--format=%ad', '--date=raw', '--',
             'blog/%s/%s' % (slug, filename),
         ]
         if reverse:
             cmd.insert(0, '--reverse')
+            # pylint: disable=no-member
             result = sh.head(self.git.log(cmd), -1)
         else:
             cmd.insert(0, '-1')
             result = self.git.log(cmd)
         if result:
             parts = result.stdout.decode().strip().split()
-            return datetime.fromtimestamp(
-                int(parts[0]),
-                timezone(
-                    timedelta(
-                        hours=int(parts[1][0:3]),
-                        minutes=int(parts[1][3:5])
-                    )
-                ),
-            )
+            return date.fromtimestamp(int(parts[0]))
         return None
 
     def _first_published(self, slug):
-        """ Return a datetime object for first_published field or None. """
-        return self._git_log_date(slug, 'content.md', True)
+        """ Return a date object for first_published field or None. """
+        return self._date(slug, 'content.md', True)
 
     def _last_update(self, slug):
-        """ Return a datetime object for last_update field or None. """
-        content_date = self._git_log_date(slug, 'content.md')
-        notes_date = self._git_log_date(slug, 'notes.md')
+        """ Return a date object for last_update field or None. """
+        content_date = self._date(slug, 'content.md')
+        notes_date = self._date(slug, 'notes.md')
         if content_date and notes_date:
             if content_date > notes_date:
                 return content_date
@@ -123,17 +101,39 @@ class Command(BaseCommand):
                 return path_fd.readline()[2:]
         return None
 
-    def _update_entry(self, entry):
+    def _update(self, entry):
         """ Update an existing blog entry. """
-        last_update = self._last_update(entry.slug)
-        if last_update:
-            lede = self._lede(entry.slug)
-            published = lede is not None
-            title = self._title(entry.slug)
-        entry.last_update = last_update
-        entry.lede = lede
-        entry.published = published
-        entry.title = title
-        entry.save()
-        self._cp_static(entry.slug)
-        print('Updated', entry)
+        data = {}
+        data['last_update'] = self._last_update(entry.slug)
+        if data['last_update']:
+            data['lede'] = self._lede(entry.slug)
+            data['published'] = data['lede'] is not None
+            data['title'] = self._title(entry.slug)
+        changed = False
+        for key, value in data.items():
+            if getattr(entry, key) != value:
+                setattr(entry, key, value)
+                changed = True
+        if changed:
+            entry.save()
+            print('Updated', entry.slug)
+
+    def handle(self, *args, **options):
+        """ Import blog data. """
+
+        # Get entry objects dict, delete objects with no dir.
+        entry_dirs = os.listdir(self.blog_dir)
+        entries = {}  # A dict of existing objects keyed by slug.
+        for entry in Entry.objects.all():
+            if entry.slug not in entry_dirs:
+                entry.delete()
+                print('Deleted', entry.slug)
+            else:
+                entries[entry.slug] = entry
+
+        # Create missing entries, update existing.
+        for slug in entry_dirs:
+            if slug in entries:
+                self._update(entries[slug])
+            else:
+                self._create(slug)
